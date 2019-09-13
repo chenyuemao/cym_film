09165fd9f253893aa3938b2efbc76a73f5e38ab2
chenyuemao <326889225@qq.com>
2019-09-14 01:44:30 +0800

    project commit

---
+++ src/main/java/com/cym/film/CymFilmApplication.java
@@ -0,0 +1,13 @@
+package com.cym.film;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CymFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CymFilmApplication.class, args);
+	}
+
+}
